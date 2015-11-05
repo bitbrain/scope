5b66a1de82720d77dfd8f67c4d0cbe69aeb57580
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-11-05 22:59:52 +0100

    Initial libgdx setup

---
+++ android/src/nl/fontys/scope/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package nl.fontys.scope.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import nl.fontys.scope.ScopeGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ScopeGame(), config);
+	}
+}
