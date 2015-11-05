acc30fab464f312b73ea4e3f356ccdeb80e37075
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-11-06 00:10:46 +0100

    Added anti aliasing

--- desktop/src/nl/fontys/scope/desktop/DesktopLauncher.java
+++ desktop/src/nl/fontys/scope/desktop/DesktopLauncher.java
@@ -9,6 +9,9 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1280;
 		config.height = 1080;
+		config.vSyncEnabled = true;
+		config.useHDPI = true;
+		config.samples = 8;
 		new LwjglApplication(new ScopeGame(), config);
 	}
 }
