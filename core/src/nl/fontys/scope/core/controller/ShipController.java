019f997dfaba1365e12461e151ed7bb3103b1a20
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-11-10 18:54:17 +0100

    Add euler angle calculation

--- core/src/nl/fontys/scope/core/controller/ShipController.java
+++ core/src/nl/fontys/scope/core/controller/ShipController.java
@@ -1,5 +1,6 @@
 package nl.fontys.scope.core.controller;
 
+import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
 
 import nl.fontys.scope.controls.Moveable;
@@ -11,23 +12,31 @@ public class ShipController implements GameObjectController, Moveable {
 
     private Vector3 accel = new Vector3();
 
+    private float angle = 0;
+
     @Override
     public void update(GameObject object, float delta) {
-
+        Quaternion q = object.getOrientation();
+        accel.z = 0;
+        accel.rotate(q.getAngleAround(Vector3.X), 1f, 0f, 0f);
+        accel.rotate(q.getAngleAround(Vector3.Y), 0f, 1f, 0f);
+        accel.rotate(q.getAngleAround(Vector3.Z), 0f, 0f, 1f);
         object.getVelocity().x += accel.x;
         object.getVelocity().y += accel.y;
         object.getVelocity().z += accel.z;
+        q.set(Vector3.Y, q.getAngleAround(Vector3.Y)  + angle);
         accel.scl(0.7f);
+        angle *= 0.98f;
     }
 
     @Override
     public void moveLeft() {
-        accel.z = -SPEED / 2f;
+        angle += 0.1f;
     }
 
     @Override
     public void moveRight() {
-        accel.z = SPEED / 2f;
+        angle += -0.1f;
     }
 
     @Override
