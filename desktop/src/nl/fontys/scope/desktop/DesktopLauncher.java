f6d4847051bd193b28e8f16c787dffd2b3a95bb7
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-11-30 14:10:09 +0100

    Add universal tween engine, closes #33

--- desktop/src/nl/fontys/scope/desktop/DesktopLauncher.java
+++ desktop/src/nl/fontys/scope/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package nl.fontys.scope.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import nl.fontys.scope.ScopeGame;
@@ -7,11 +8,11 @@ import nl.fontys.scope.ScopeGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1920;
-		config.height = 1080;
 		config.vSyncEnabled = true;
 		config.useHDPI = true;
 		config.fullscreen = true;
+		config.width = 1366;
+		config.height = 768;
 		new LwjglApplication(new ScopeGame(), config);
 	}
 }
