970734151d1f923f14d790c811d47ec6d922f58d
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-12-02 13:37:51 +0100

    Added forgotten class

---
+++ core/src/nl/fontys/scope/screens/MenuScreen.java
@@ -0,0 +1,27 @@
+package nl.fontys.scope.screens;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+import nl.fontys.scope.ScopeGame;
+
+public class MenuScreen extends AbstractScreen {
+
+    public MenuScreen(ScopeGame game) {
+        super(game);
+    }
+
+    @Override
+    protected void onShow() {
+
+    }
+
+    @Override
+    protected void onUpdate(float delta) {
+
+    }
+
+    @Override
+    protected void onCreateStage(Stage stage) {
+
+    }
+}
