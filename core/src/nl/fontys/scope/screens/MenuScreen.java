5fe83c1130c9168ffc77ea0ec8d6f904b0432531
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-12-03 09:34:42 +0100

    Configure custom shaders for main menu

--- core/src/nl/fontys/scope/screens/MenuScreen.java
+++ core/src/nl/fontys/scope/screens/MenuScreen.java
@@ -1,6 +1,7 @@
 package nl.fontys.scope.screens;
 
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.bitfire.postprocessing.filters.Blur;
 
 import nl.fontys.scope.ScopeGame;
 import nl.fontys.scope.core.controller.CameraRotatingController;
@@ -19,6 +20,9 @@ public class MenuScreen extends AbstractScreen {
     protected void onShow() {
         GameObject planet = factory.createPlanet(0f, 50f, 0f, 0f);
         world.addController(new CameraRotatingController(500f, world.getCamera(), planet));
+        shaderManager.bloom.setBaseSaturation(3f);
+        shaderManager.bloom.setBaseIntesity(0.4f);
+        shaderManager.bloom.setBloomIntesity(4f);
     }
 
     @Override
