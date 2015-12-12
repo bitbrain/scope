36eb640523d7f6553ea7e1e0268695e4c5e4a037
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-12-12 22:54:51 +0100

    #17: players now support points

--- core/src/nl/fontys/scope/core/Player.java
+++ core/src/nl/fontys/scope/core/Player.java
@@ -8,8 +8,34 @@ public class Player {
 
     private GameObject ship;
 
+    private int energyCount;
+
+    private int points;
+
     private Player() {
 
+    }
+
+    public int getEnergyCount() {
+        return energyCount;
+    }
+
+    public void addEnergy() {
+        energyCount++;
+    }
+
+    public int dropEnergy() {
+        int count = energyCount;
+        energyCount = 0;
+        return count;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void addPoints(int points) {
+        this.points = points;
     }
 
     public static void setCurrent(GameObject ship) {
