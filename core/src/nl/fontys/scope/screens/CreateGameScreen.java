26d8ddceda33a96d7c1377729117ba8c4f9951d0
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-12-14 12:21:51 +0100

    Add basic version of create game layout

--- core/src/nl/fontys/scope/screens/CreateGameScreen.java
+++ core/src/nl/fontys/scope/screens/CreateGameScreen.java
@@ -2,15 +2,20 @@ package nl.fontys.scope.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import nl.fontys.scope.ScopeGame;
 import nl.fontys.scope.core.controller.CameraRotatingController;
 import nl.fontys.scope.i18n.Bundle;
 import nl.fontys.scope.i18n.Messages;
 import nl.fontys.scope.object.GameObject;
+import nl.fontys.scope.ui.ButtonMenu;
 import nl.fontys.scope.ui.Styles;
 
 public class CreateGameScreen extends AbstractScreen {
@@ -38,8 +43,23 @@ public class CreateGameScreen extends AbstractScreen {
         Table layout = new Table();
         layout.setFillParent(true);
         Label caption = new Label(Bundle.general.get(Messages.MENU_NEW_GAME), Styles.LABEL_CAPTION);
-        layout.add(caption).row();
+        layout.add(caption).padBottom(55f).row();
 
+        // Name of the game
+        Label name = new Label("Name", Styles.LABEL_DESCRIPTION);
+        layout.add(name).row();
+        TextField nameText = new TextField("Enter name...", Styles.TEXTFIELD_FORM);
+        layout.add(nameText).row();
+
+        ButtonMenu menu = new ButtonMenu(tweenManager);
+        menu.add("Create", new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                setScreen(new IngameScreen(game, false));
+            }
+        });
+        layout.add(menu).padTop(30f).row();
         stage.addActor(layout);
     }
 }
--- core/src/nl/fontys/scope/ui/Styles.java
+++ core/src/nl/fontys/scope/ui/Styles.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 
@@ -18,8 +19,11 @@ public final class Styles {
     public static final Label.LabelStyle LABEL_CREDITS = new Label.LabelStyle();
     public static final Label.LabelStyle LABEL_VERSION = new Label.LabelStyle();
     public static final Label.LabelStyle LABEL_CAPTION = new Label.LabelStyle();
+    public static final Label.LabelStyle LABEL_DESCRIPTION = new Label.LabelStyle();
 
     public static final TextButton.TextButtonStyle BUTTON_MENU = new TextButton.TextButtonStyle();
+
+    public static final TextField.TextFieldStyle TEXTFIELD_FORM = new TextField.TextFieldStyle();
 
     public static void init() {
         LABEL_DEBUG.font = AssetManager.getFont(Assets.Fonts.DEBUG_20);
@@ -38,11 +42,20 @@ public final class Styles {
         LABEL_CAPTION.font = AssetManager.getFont(Assets.Fonts.OPENSANS_LARGE_64);
         LABEL_CAPTION.fontColor = Colors.PRIMARY.cpy();
 
+        LABEL_DESCRIPTION.font = AssetManager.getFont(Assets.Fonts.OPENSANS_MEDIUM_32);
+        LABEL_DESCRIPTION.fontColor = Colors.PRIMARY_ADDITION.cpy();
+
         BUTTON_MENU.font = AssetManager.getFont(Assets.Fonts.OPENSANS_MEDIUM_32);
         BUTTON_MENU.fontColor = Colors.PRIMARY.cpy();
         BUTTON_MENU.overFontColor = Color.ORANGE.cpy();
         BUTTON_MENU.up = new NinePatchDrawable(GraphicsFactory.createNinePatch(Assets.Textures.BUTTON, 40));
         BUTTON_MENU.down = new NinePatchDrawable(GraphicsFactory.createNinePatch(Assets.Textures.BUTTON_ACTIVE, 40));
         BUTTON_MENU.over = new NinePatchDrawable(GraphicsFactory.createNinePatch(Assets.Textures.BUTTON_ACTIVE, 40));
+
+        TEXTFIELD_FORM.font = AssetManager.getFont(Assets.Fonts.OPENSANS_MEDIUM_32);
+        TEXTFIELD_FORM.messageFont = AssetManager.getFont(Assets.Fonts.OPENSANS_MEDIUM_32);
+        TEXTFIELD_FORM.fontColor = Colors.PRIMARY;
+        TEXTFIELD_FORM.focusedFontColor = Color.ORANGE;
+        TEXTFIELD_FORM.messageFontColor = Colors.PRIMARY_ADDITION;
     }
 }
