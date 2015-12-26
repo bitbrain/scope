c525a747acdd7317efe5a207cdcd32ed18a8bfa2
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-12-26 01:34:22 +0100

    Reduced widget alpha

--- core/src/nl/fontys/scope/ui/FocusWidget.java
+++ core/src/nl/fontys/scope/ui/FocusWidget.java
@@ -120,7 +120,7 @@ public class FocusWidget extends Actor {
         if (focusData.visible) {
             focusData.visible = false;
             tweenManager.killTarget(focusData.value);
-            Tween.to(focusData.value, ValueTween.VALUE, TRANSITION_TIME).target(0.3f).ease(TweenEquations.easeOutCubic).start(tweenManager);
+            Tween.to(focusData.value, ValueTween.VALUE, TRANSITION_TIME).target(0.2f).ease(TweenEquations.easeOutCubic).start(tweenManager);
         }
         parentAlpha = focusData.value.getValue();
         vecTemp2D.set(cameraPos2D.x - Gdx.graphics.getWidth() / 2f, cameraPos2D.y - Gdx.graphics.getHeight() / 2f);
