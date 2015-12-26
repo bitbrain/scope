ab7b3924511599963c34018a0f21739768359af6
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-12-26 22:57:29 +0100

    Set ACTIVE color more dominant

--- core/src/nl/fontys/scope/util/Colors.java
+++ core/src/nl/fontys/scope/util/Colors.java
@@ -7,7 +7,7 @@ public final class Colors {
     public static final Color PRIMARY = Color.valueOf("00ff5a");
     public static final Color SECONDARY = Color.valueOf("28ffb1");
     public static final Color BACKGROUND = Color.valueOf("0e0217");
-    public static final Color ACTIVE = Color.valueOf("ff7800");
+    public static final Color ACTIVE = Color.valueOf("ff5a00");
     public static final Color UI = Color.valueOf("00ffa2");
 
     public static Color trans(Color color, float alpha) {
