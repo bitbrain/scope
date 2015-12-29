bc079528a3aa76c462e6e9dc2f586b802da42efa
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-12-29 01:51:41 +0100

    Increase version number, closes #27

--- core/src/nl/fontys/scope/Config.java
+++ core/src/nl/fontys/scope/Config.java
@@ -3,7 +3,7 @@ package nl.fontys.scope;
 public class Config {
 
     public static final String APP_NAME = "scope";
-    public static final String APP_VERSION = "0.5.0";
+    public static final String APP_VERSION = "0.5.1";
 
     public static final String FLAG_DEBUG = "-debug";
 }
