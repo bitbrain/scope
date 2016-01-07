878abc20abbe8a602d6a44af4eba74f18a8d63ea
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-01-07 17:59:53 +0100

    Set shoot action on Xbox to LT key

--- core/src/nl/fontys/scope/controls/XBoxControllerSupport.java
+++ core/src/nl/fontys/scope/controls/XBoxControllerSupport.java
@@ -24,16 +24,21 @@ class XBoxControllerSupport extends ControllerSupport {
 
     public XBoxControllerSupport(Moveable moveable) {
         super(moveable);
-        register(Buttons.RIGHT_STICK, MoveableAction.SHOOT);
     }
 
     @Override
     protected void onUpdate() {
-        act(MoveableAction.BOOST, Buttons.T_TRIGGER_CODE, 0.02f);
-        act(MoveableAction.RISE, Buttons.RIGHT_STICK_CODE_Y, 0.02f);
+        float value = getAxisValue(Buttons.T_TRIGGER_CODE);
+        final float TOLERANCE = 0.02f;
+        if (value > TOLERANCE) {
+            MoveableAction.SHOOT.act(moveable);
+        } else {
+            act(MoveableAction.BOOST, Buttons.T_TRIGGER_CODE, TOLERANCE);
+        }
+        act(MoveableAction.RISE, Buttons.RIGHT_STICK_CODE_Y, TOLERANCE);
 
-        final float ROTATION_SPEED = 1.9f;
-        float value = getAxisValue(Buttons.LEFT_STICK_CODE_X);
+        final float ROTATION_SPEED = 1.5f;
+        value = getAxisValue(Buttons.LEFT_STICK_CODE_X);
         if (value > 0.2f || value < -0.2f) {
             MoveableAction.ROTATE.act(moveable, value * ROTATION_SPEED, 0f, 0f);
         }
