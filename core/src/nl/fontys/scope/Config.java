db679c80ce3eb836eee328186461cf0bd8c0ad5d
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-01-07 18:19:07 +0100

    Increase version to 0.6.7

--- core/src/nl/fontys/scope/Config.java
+++ core/src/nl/fontys/scope/Config.java
@@ -6,7 +6,7 @@ public class Config {
     public static final String APP_NAME = "scope";
 
     // Version of the application
-    public static final String APP_VERSION = "0.6.2";
+    public static final String APP_VERSION = "0.6.7";
 
     // console string of the debug statement
     public static final String FLAG_DEBUG = "-debug";
