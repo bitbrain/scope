fa3812b96232073c56c1023665727e2fb11df155
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-01-09 23:49:48 +0100

    Set screen size to full only in FULLSCREEN mode :tv:

--- core/src/nl/fontys/scope/screens/ScreenHandler.java
+++ core/src/nl/fontys/scope/screens/ScreenHandler.java
@@ -7,8 +7,8 @@ import nl.fontys.scope.Config;
 public final class ScreenHandler {
 
     public static void setFull() {
-        Gdx.graphics.setWindowedMode(Gdx.graphics.getDisplayMode().width, Gdx.graphics.getDisplayMode().height);
         if (Config.AUTO_FULLSCREEN) {
+            Gdx.graphics.setWindowedMode(Gdx.graphics.getDisplayMode().width, Gdx.graphics.getDisplayMode().height);
             Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
         }
 
