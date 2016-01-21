1ba6923a2bf5081ba73f0a561b9d716b3d71a710
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-01-21 18:39:19 +0100

    Initial version of singleplayer screen

--- core/src/nl/fontys/scope/screens/SingleplayerScreen.java
+++ core/src/nl/fontys/scope/screens/SingleplayerScreen.java
@@ -1,9 +1,12 @@
 package nl.fontys.scope.screens;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import nl.fontys.scope.ScopeGame;
@@ -11,6 +14,7 @@ import nl.fontys.scope.core.Player;
 import nl.fontys.scope.core.PlayerManager;
 import nl.fontys.scope.core.World;
 import nl.fontys.scope.core.logic.AILogic;
+import nl.fontys.scope.core.logic.ai.Difficulty;
 import nl.fontys.scope.i18n.Bundle;
 import nl.fontys.scope.i18n.Messages;
 import nl.fontys.scope.ui.ButtonMenu;
@@ -18,6 +22,33 @@ import nl.fontys.scope.ui.ExitHandler;
 import nl.fontys.scope.ui.Styles;
 
 public class SingleplayerScreen extends AbstractScreen implements ExitHandler {
+
+    private PlayerTable playerTable;
+
+    private class AIConfig {
+        public Color color;
+        public Difficulty difficulty;
+    }
+
+    private class PlayerTable extends Table {
+
+        public PlayerTable() {
+            addPlayerRow();
+            addPlayerRow();
+            addPlayerRow();
+            addPlayerRow();
+        }
+
+        private void addPlayerRow() {
+            Table row = new Table();
+            row.add(new TextButton("AI", Styles.BUTTON_MENU)).width(90f).height(90f).padRight(30f);
+            TextField name = new TextField("Player" + (getChildren().size + 1), Styles.TEXTFIELD_FORM);
+            row.add(name).width(200f).height(90f).padRight(20f);
+            row.add(new TextButton("Difficulty", Styles.BUTTON_MENU)).height(90f).padRight(30f);
+            row.add(new TextButton("Color", Styles.BUTTON_MENU)).height(90f).padRight(30f);
+            add(row).padBottom(15f).row();
+        }
+    }
 
     private ButtonMenu menu;
 
@@ -41,6 +72,8 @@ public class SingleplayerScreen extends AbstractScreen implements ExitHandler {
         layout.setFillParent(true);
         Label caption = new Label(Bundle.general.get(Messages.MENU_SINGLE_PLAYER), Styles.LABEL_CAPTION);
         layout.add(caption).padBottom(55f).row();
+        playerTable = new PlayerTable();
+        layout.add(playerTable).row();
         menu = new ButtonMenu(tweenManager);
         menu.add(Bundle.general.get(Messages.GAME_JOIN), new ClickListener() {
             @Override
