e39f95ab03ffcc875a0b2fbe04d81c7de0466160
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-01-26 02:54:04 +0100

    #68: initial version of model preview

---
+++ core/src/nl/fontys/scope/ui/ModelPreview.java
@@ -0,0 +1,98 @@
+package nl.fontys.scope.ui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.PointLight;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+import nl.fontys.scope.util.Colors;
+
+public class ModelPreview extends Actor {
+
+    private ModelInstance instance;
+
+    private ModelBatch batch;
+
+    private Environment environment;
+
+    private PerspectiveCamera camera;
+
+    private FrameBuffer buffer;
+
+    public ModelPreview(Model model, float width, float height) {
+        this.instance = new ModelInstance(model);
+        this.batch = new ModelBatch();
+        this.environment = new Environment();
+        this.camera = new PerspectiveCamera(40f, width, height);
+        setupCamera();
+        setupEnvironment();
+        setSize(width, height);
+    }
+
+    @Override
+    public void setSize(float width, float height) {
+        super.setSize(width, height);
+        revalidateFrameBuffer(width, height);
+    }
+
+    @Override
+    public void setWidth(float width) {
+        super.setWidth(width);
+        revalidateFrameBuffer(width, getHeight());
+    }
+
+    @Override
+    public void setHeight(float height) {
+        super.setHeight(height);
+        revalidateFrameBuffer(getWidth(), height);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        camera.update();
+        batch.end();
+        buffer.begin();
+        Gdx.gl.glClearColor(0f, 0f, 0f, 0f);
+        Gdx.gl.glViewport(0, 0, (int) getWidth(), (int) getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT | (Gdx.graphics.getBufferFormat().coverageSampling ? GL20.GL_COVERAGE_BUFFER_BIT_NV : 0));
+        this.batch.begin(camera);
+        this.batch.render(instance, environment);
+        instance.transform.rotate(Vector3.Y, 2f);
+        this.batch.end();
+        buffer.end();
+        batch.begin();
+        batch.draw(buffer.getColorBufferTexture(), getX(), getY(), getWidth(), getHeight());
+    }
+
+    private void revalidateFrameBuffer(float width, float height) {
+        if (buffer != null) {
+            buffer.dispose();
+        }
+        buffer = new FrameBuffer(Pixmap.Format.RGBA4444, (int)width, (int)height, false);
+        camera.viewportWidth = width;
+        camera.viewportHeight = height;
+    }
+
+    private void setupCamera() {
+        camera.position.set(4f, 4f, 4f);
+        camera.near = 0.2f;
+        camera.far = 30000f;
+        camera.lookAt(0f, 0f, 0f);
+        camera.up.set(0f, -1f, 0f);
+    }
+
+    private void setupEnvironment() {
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.2f, 0.6f, 1f));
+        environment.add(new PointLight().set(Colors.PRIMARY, 5f, 5f, 5f, 25));
+    }
+}
