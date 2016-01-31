77dc78d211d67ea3a708d43fb96a35b7004d6624
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-01-31 15:38:06 +0100

    #62: AI moves to sphere when progress is over 0.9

--- core/src/nl/fontys/scope/core/logic/ai/AimSphereTask.java
+++ core/src/nl/fontys/scope/core/logic/ai/AimSphereTask.java
@@ -1,9 +1,17 @@
 package nl.fontys.scope.core.logic.ai;
 
+import nl.fontys.scope.core.Player;
+import nl.fontys.scope.util.GameObjectUtil;
+
 public class AimSphereTask extends AITask {
 
     @Override
     public Status execute() {
-        return super.execute();
+        AIState state = getObject();
+        Player player = state.player;
+        if (player.getFocusProgress() > 0.9f) {
+            GameObjectUtil.moveToTarget(player.getShip(), state.sphere, state.delta);
+        }
+        return Status.FAILED;
     }
 }
