fa9cb74a023450b1269e8b784f8c1e4affd3279d
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-02-14 00:58:03 +0100

    Add new gameserver class

---
+++ core/src/nl/fontys/scope/net/GameServer.java
@@ -0,0 +1,49 @@
+package nl.fontys.scope.net;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Logger;
+
+/**
+ * Game server class (standalone) which handles all the network logic
+ */
+public class GameServer {
+
+    public static final Logger LOGGER = Logger.getLogger(GameServer.class.getName());
+
+    private AtomicBoolean running = new AtomicBoolean(false);
+
+    /**
+     * Starts the server (initialization)
+     */
+    public void start() {
+        LOGGER.info("Starting game server..");
+        // TODO
+        running.set(true);
+        LOGGER.info("Started!");
+    }
+
+    /**
+     * Passes a simple command to the server
+     */
+    public void command(String command) {
+        LOGGER.info("Passing command: '" + command + "'");
+        // TODO
+        if (command.equals("stop")) {
+            stop();
+        }
+    }
+
+    /**
+     * Stops the server (shutdown)
+     */
+    public void stop() {
+        LOGGER.info("Stopping game server..");
+        // TODO
+        running.set(false);
+        LOGGER.info("Stopped!");
+    }
+
+    public boolean isRunning() {
+        return running.get();
+    }
+}
