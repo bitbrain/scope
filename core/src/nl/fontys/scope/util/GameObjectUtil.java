f49720fc45c8b2baf5a03bfdff20fa4b737dab55
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-02-13 23:22:52 +0100

    Make GameObjectUtils more secure

--- core/src/nl/fontys/scope/util/GameObjectUtil.java
+++ core/src/nl/fontys/scope/util/GameObjectUtil.java
@@ -12,22 +12,30 @@ public final class GameObjectUtil {
     private static Vector3 v = new Vector3();
 
     public static void moveToTarget(GameObject thisObject, GameObject targetObject, float delta) {
-        v.set(targetObject.getPosition()).sub(thisObject.getPosition());
-        v.nor();
-        thisObject.getVelocity().add(v.scl(95f * delta));
-        rotateTo(thisObject, targetObject);
+        if (thisObject != null && targetObject != null) {
+            v.set(targetObject.getPosition()).sub(thisObject.getPosition());
+            v.nor();
+            thisObject.getVelocity().add(v.scl(95f * delta));
+            rotateTo(thisObject, targetObject);
+        }
     }
 
     public static void rotateTo(GameObject thisObject, GameObject targetObject) {
-        v.set(targetObject.getPosition());
-        v.set(v.sub(thisObject.getPosition()));
-        v.nor();
-        mat.set(v.x, v.y, v.z, v.len());
-        thisObject.getOrientation().setFromMatrix(mat);
+        if (thisObject != null && targetObject != null) {
+            v.set(targetObject.getPosition());
+            v.set(v.sub(thisObject.getPosition()));
+            v.nor();
+            mat.set(v.x, v.y, v.z, v.len());
+            thisObject.getOrientation().setFromMatrix(mat);
+        }
     }
 
     public static float distanceTo(GameObject thisObject, GameObject targetObject) {
-        v.set(targetObject.getPosition());
-        return v.sub(thisObject.getPosition()).len();
+        if (thisObject != null && targetObject != null) {
+            v.set(targetObject.getPosition());
+            return v.sub(thisObject.getPosition()).len();
+        } else {
+            return 0f;
+        }
     }
 }
