47035f35b4e3e1a89d68c96638c33b3af6a69aa8
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-02-21 17:49:01 +0100

    #76: add metadata to request classes

--- core/src/nl/fontys/scope/net/client/Requests.java
+++ core/src/nl/fontys/scope/net/client/Requests.java
@@ -1,5 +1,8 @@
 package nl.fontys.scope.net.client;
 
+import nl.fontys.scope.core.Player;
+import nl.fontys.scope.object.GameObject;
+
 public class Requests {
 
     // list of all requests
@@ -34,29 +37,57 @@ public class Requests {
 
     public static class AddObject extends Request {
 
-        public AddObject(String gameId, String clientId) {
+        private GameObject object;
+
+        public AddObject(String gameId, String clientId, GameObject object) {
             super(gameId, clientId);
+            this.object = object;
+        }
+
+        public GameObject getGameObject() {
+            return object;
         }
     }
 
     public static class RemoveObject extends Request {
 
-        public RemoveObject(String gameId, String clientId) {
+        private String gameObjectId;
+
+        public RemoveObject(String gameId, String clientId, String gameObjectId) {
             super(gameId, clientId);
+            this.gameObjectId = gameObjectId;
+        }
+
+        public String getGameObjectId() {
+            return gameObjectId;
         }
     }
 
     public static class UpdateObject extends Request {
 
-        public UpdateObject(String gameId, String clientId) {
+        private GameObject object;
+
+        public UpdateObject(String gameId, String clientId, GameObject object) {
             super(gameId, clientId);
+            this.object = object;
+        }
+
+        public GameObject getGameObject() {
+            return object;
         }
     }
 
     public static class UpdatePlayer extends Request {
 
-        public UpdatePlayer(String gameId, String clientId) {
+        private Player player;
+
+        public UpdatePlayer(String gameId, String clientId, Player player) {
             super(gameId, clientId);
+            this.player = player;
+        }
+
+        public Player getPlayer() {
+            return player;
         }
     }
 
