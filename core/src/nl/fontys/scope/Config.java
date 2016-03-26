e462c5078abe1a7e0b61aeaf50296df24935e271
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2016-03-26 23:37:01 +0100

    Refactor Config to use implicit interface constants

--- core/src/nl/fontys/scope/Config.java
+++ core/src/nl/fontys/scope/Config.java
@@ -1,84 +1,84 @@
 package nl.fontys.scope;
 
-public class Config {
+public interface Config {
 
     // Name of the application
-    public static final String APP_NAME = "scope";
+    String APP_NAME = "scope";
 
     // Version of the application
-    public static final String APP_VERSION = "0.9.3";
+    String APP_VERSION = "0.9.3";
 
     // console string of the debug statement
-    public static final String FLAG_DEBUG = "-debug";
+    String FLAG_DEBUG = "-debug";
 
-    public static final boolean NETWORKING_ENABLED = false;
+    boolean NETWORKING_ENABLED = false;
 
     // Set to false if game should start not in fullscreen mode
-    public static final boolean AUTO_FULLSCREEN = true;
+    boolean AUTO_FULLSCREEN = true;
 
     // Maximum distance for panning
-    public static final float MAX_PAN_DISTANCE = 50f;
+    float MAX_PAN_DISTANCE = 50f;
 
     // Maximum distance for volume
-    public static final float MAX_VOLUME_DISTANCE = 100f;
+    float MAX_VOLUME_DISTANCE = 100f;
 
     // Maximum energy speed
-    public static final float MAX_ENERGY_SPEED = 28f;
+    float MAX_ENERGY_SPEED = 28f;
 
     // Default energy count
-    public static final int ENERGY_COUNT = 25;
+    int ENERGY_COUNT = 25;
 
     // Arena radius
-    public static final float ARENA_RADIUS = 140;
+    float ARENA_RADIUS = 140;
 
     // Arena radius (outer)
-    public static final float ARENA_OUTER_RADIUS = 100;
+    float ARENA_OUTER_RADIUS = 100;
 
     // Soft gap of focus (100% approach)
-    public static final int FOCUS_SOFT_GAP = 16;
+    int FOCUS_SOFT_GAP = 16;
 
     // Points player gets per energy
-    public static final int POINTS_PER_ENERGY = 18;
+    int POINTS_PER_ENERGY = 18;
 
     // Points to win the game
-    public static final int POINT_GAP = 500;
+    int POINT_GAP = 500;
 
     // Number of munitions per energy consumption
-    public static final int MUNITION_GAP = 5;
+    int MUNITION_GAP = 5;
 
     // Width of a menu button
-    public static final float MENU_BUTTON_WIDTH = 300f;
+    float MENU_BUTTON_WIDTH = 300f;
 
     // Height of a menu button
-    public static final float MENU_BUTTON_HEIGHT = 90f;
+    float MENU_BUTTON_HEIGHT = 90f;
 
     // Padding between menu buttons
-    public static final float MENU_PADDING = 10f;
+    float MENU_PADDING = 10f;
 
     // Initial alpha of menu
-    public static final float MENU_ALPHA = 0.5f;
+    float MENU_ALPHA = 0.5f;
 
     // Bar padding
-    public static final float UI_BAR_PADDING = 4f;
+    float UI_BAR_PADDING = 4f;
 
     // Offset of tooltips (vertical)
-    public static final float TOOLTIP_VERTICAL_OFFSET = 75f;
+    float TOOLTIP_VERTICAL_OFFSET = 75f;
 
     // Margin of UI shadows
-    public static final float UI_SHADOW_MARGIN = 64f;
+    float UI_SHADOW_MARGIN = 64f;
 
     // High quality rendering
-    public static final boolean HIGH_QUALITY = true;
+    boolean HIGH_QUALITY = true;
 
     // The IP the game client should connect to
-    public static final String SERVER_IP = "127.0.0.1";
+    String SERVER_IP = "127.0.0.1";
 
     // Timeout for server connection in miliseconds
-    public static final int SERVER_TIMEOUT_MS = 5000;
+    int SERVER_TIMEOUT_MS = 5000;
 
     // UDP port of the server
-    public static final int SERVER_UDP_PORT = 54555;
+    int SERVER_UDP_PORT = 54555;
 
     // TCP port of the server
-    public static final int SERVER_TCP_PORT = 54777;
+    int SERVER_TCP_PORT = 54777;
 }
